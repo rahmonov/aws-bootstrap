29f24ac06b6272593d7ed6f701e1722a2bd62469
Jahongir Rahmonov <jrahmonov2@gmail.com>
2021-06-10 19:30:33 +0200

    Change hello world message

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -9,6 +9,11 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 
 # Deploy static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
@@ -33,7 +38,12 @@ aws cloudformation deploy \
 	--no-fail-on-empty-changeset \
 	--capabilities CAPABILITY_NAMED_IAM \
 	--parameter-overrides \
-		EC2InstanceType=$EC2_INSTANCE_TYPE
+		EC2InstanceType=$EC2_INSTANCE_TYPE \
+		GitHubOwner=$GH_OWNER \
+		GitHubRepo=$GH_REPO \
+		GitHubBranch=$GH_BRANCH \
+		GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+		CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 
 # If the deploy succeeded, show the DNS name of the created instance
